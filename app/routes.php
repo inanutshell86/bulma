5baa7ef622365ec35fd31e9edbd54bc1ee737b00
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-07-05 23:17:16 +0300

    Added FastRoute\Routecollector component and added basic routes.

--- app/routes.php
+++ app/routes.php
@@ -1,1 +1,75 @@
 <?php
+
+use Aura\SqlQuery\QueryFactory;
+use DI\ContainerBuilder;
+use FastRoute\RouteCollector;
+use League\Plates\Engine;
+use Illuminate\Support;
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions([
+    Engine::class => function() {
+        return new Engine('../app/Views');
+    },
+
+    PDO::class => function() {
+        $driver = config('database.driver');
+        $host = config('database.host');
+        $database_name = config('database.database_name');
+        $username = config('database.username');
+        $password = config('database.password');
+
+        return new PDO("$driver:host=$host;dbname=$database_name", $username, $password);
+    },
+
+    QueryFactory::class => function() {
+        return new QueryFactory('mysql');
+    }
+]);
+
+$container = $containerBuilder->build();
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->get('/', ['App\Controllers\HomeController', 'index']);
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        abort(404);
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        dd('The query method is not allowed.');
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        $container->call($handler, $vars);
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
