7ef59ed9aad8bd7875c93bd71f2f81a0488ef5ca
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-07-05 18:57:45 +0300

    Building the ImageManager model.

---
+++ app/Models/ImageManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Intervention\Image\ImageManagerStatic as Image;
+
+class ImageManager
+{
+    private $folder;
+
+    public function __construct()
+    {
+        $this->folder = config('uploadsFolder');
+    }
+
+    public function checkImageExist($path)
+    {
+        if ($path != null && is_file($this->folder . $path) && file_exists($this->folder . $path)) {
+            return true;
+        }
+    }
+
+    public function uploadImage($img, $currImg = null)
+    {
+        if (!is_file($img['tmp_name']) && !is_uploaded_file($img['tmp_name'])) {
+            return $currImg;
+        }
+
+        $this->deleteImg($currImg);
+        $filename = strtolower(uniqid('', true) . '.' . pathinfo($img['name'], PATHINFO_EXTENSION));
+        $img = Image::make($img['tmp_name']);
+        $img->save($this->folder . $filename);
+
+        return $filename;
+    }
+
+    public function deleteImg($img)
+    {
+        if ($this->checkImageExist($img)) {
+            unlink($this->folder . $img);
+        }
+    }
+
+    public function getImg($img)
+    {
+        if ($this->checkImageExist($img)) {
+            return '/' . $this->folder . $img;
+        }
+
+        return '/img/no-user.png';
+    }
+
+}
